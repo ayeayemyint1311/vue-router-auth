8dd0d96f757ec0cf942f036ddcbdc3248d711a7a
Aye Aye Myint <ayeayemyint.mm1999@gmail.com>
2025-02-20 13:56:36 +0630

    Login Form with tailwindcss

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,65 @@
 <template>
-  <div><h1 class="text-red-500">this is login page</h1></div>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold text-center text-gray-800">Login</h2>
+      <form @submit.prevent="handleLogin" class="mt-6">
+        <div>
+          <label class="block text-gray-700">Email</label>
+          <input
+            type="email"
+            v-model="formData.email"
+            class="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+
+        <div class="mt-4">
+          <label class="block text-gray-700">Password</label>
+          <input
+            type="password"
+            v-model="formData.password"
+            class="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+
+        <div class="mt-4 flex items-center justify-between">
+          <label class="flex items-center">
+            <input type="checkbox" class="mr-2 rounded" />
+            <span class="text-gray-600 text-sm">Remember me</span>
+          </label>
+          <a href="#" class="text-sm text-blue-600 hover:underline"
+            >Forgot Password?</a
+          >
+        </div>
+
+        <button
+          type="submit"
+          class="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 mt-6"
+        >
+          Login
+        </button>
+      </form>
+
+      <p class="mt-4 text-center text-sm text-gray-600">
+        Don't have an account?
+        <a href="#" class="text-blue-500 hover:underline">Sign up</a>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
 
-<style lang="scss" scoped></style>
+const formData = reactive({
+  name: "",
+  email: "",
+});
+
+const handleLogin = () => {
+  console.log(formData);
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E40AF", // Custom primary color
+        secondary: "#9333EA", // Custom secondary color
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Custom font
+      },
+      container: {
+        center: true,
+        padding: "5rem",
+        margin: "5rem",
+      },
+    },
   },
   plugins: [],
 };
